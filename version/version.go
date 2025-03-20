47a863c1bfb8719d40a491ca0658e230691e4a92
REW <rew.bmw@gmail.com>
2025-03-20 15:02:02 +0300

    entropyxctl v1.15.2

--- version/version.go
+++ version/version.go
@@ -10,8 +10,8 @@ const validCharacters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrs
 
 const (
 	appMajor uint = 1
-	appMinor uint = 0
-	appPatch uint = 0
+	appMinor uint = 15
+	appPatch uint = 2
 )
 
 // appBuild is defined as a variable so it can be overridden during the build
